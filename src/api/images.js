2b6a174d45dcaa752109e4e3994fad69af8f88d7
Denis Golovin <golovin.denis@pandats.com>
2020-09-15 23:01:28 +0300

    Move key to config

--- src/api/images.js
+++ src/api/images.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import * as config from '../config.json';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 export const getImages = (query, page) => {
-    const url = `/?q=${query}&page=${page}&key=18060216-54e3ca58d741e1dcf99f0c9a1&image_type=photo&orientation=horizontal&per_page=12`;
+    const url = `/?q=${query}&page=${page}&key=${config.api_key}&image_type=photo&orientation=horizontal&per_page=12`;
     return axios.get(url).then(r => {
         return r.data.hits;
     }).catch(e => {
